2c73d9f132ade7aa8542a1b93c8f2fe8f3c27bd4
aar-atiq <aar.msdeveloper01@gmail.com>
2021-03-31 05:19:48 +0600

    added imgBB api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Orders from './Components/Orders/Orders';
+import Admin from './Components/Admin/Admin';
+import Checkout from './Components/Checkout/Checkout';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Orders</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>
+              <Link to="/users">Deals</Link>
+            </li>
+            <li>
+              <Link to="/users">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <Orders />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/users">
+            <Checkout />
+          </Route>
+          <Route path="/users">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,9 +1,33 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Admin = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+    
+    const handleImageChange = event =>{
+        const imageData = new FormData();
+        imageData.set('key', '08b10d29ebe980b7653915a407c6fa7e');
+        imageData.append('image', event.target.files[0]);
+
+        axios.post('https://api.imgbb.com/1/upload', imageData)
+        .then(function(response){
+            console.log(response.data.data.display_url);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
     return (
         <div>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                
+                <input name="example" defaultValue="test" ref={register} />
+                <input name="exampleRequired" type="file" onChange={handleImageChange} />
+                <input type="submit" />
+                
+            </form>
         </div>
     );
 };
