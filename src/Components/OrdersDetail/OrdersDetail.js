2ad142cf2025dd6674cf5defac095080badbfeba
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-02 19:20:37 +0600

    update order ui

--- src/Components/OrdersDetail/OrdersDetail.js
+++ src/Components/OrdersDetail/OrdersDetail.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './OrdersDetail.css';
+import { Table } from 'react-bootstrap';
 
 const OrdersDetail = (props) => {
     const { name, author, price } = props.order.orderProduct;
     const { orderTime, _id } = props.order;
-    
-    const deleteUser = (event, id) => {
-        fetch(`http://localhost:5000//orderDelete/${id}`,{
-            method: 'DELETE'
-        })
-        .then(res => res.json())
-        .then(result =>{
-            // console.log("deleted successfully");
 
-            if(result){
-                event.target.parentNode.style.display = 'none';
-            }
-        })
+    const deleteUser = (event, id) => {
+        // fetch(`http://localhost:5000//orderDelete/${id}`,{
+        //     method: 'DELETE'
+        // })
+        // .then(res => res.json())
+        // .then(result =>{
+        //     // console.log("deleted successfully");
+
+        //     if(result){
+        //         event.target.parentNode.style.display = 'none';
+        //     }
+        // })
     }
     return (
         <div>
@@ -34,10 +35,33 @@ const OrdersDetail = (props) => {
                     <td>{orderTime}</td>
                     <td>${price}</td>
                     <td>
-                    <button onClick={deleteUser("event", _id)}>Delete</button>
+                        <button class="btn btn-danger"onClick={deleteUser()}>Delete</button>
                     </td>
                 </tr>
             </table>
+
+            {/* <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>BookName</th>
+                        <th>AuthorName</th>
+                        <th>OrderTime</th>
+                        <th>Price</th>
+                        <th>Delete Order</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{name}</td>
+                        <td>{author}</td>
+                        <td>{orderTime}</td>
+                        <td>${price}</td>
+                        <td>
+                            <button onClick={deleteUser()}>Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table> */}
         </div>
     );
 };
